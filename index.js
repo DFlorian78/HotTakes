5f9bb8de4656bf2a95279ecee7312e7097a1043c
FlorianD <100361045+DFlorian78@users.noreply.github.com>
2022-08-23 16:50:51 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,33 @@
 /// Dotenv est une dépendance pour cacher nos infos perso
 require('dotenv').config()
+///// Routes pour nos routers
+const {saucesRouter} = require("./routers/sauces.router")
+const {authRouter} = require("./routers/auth.router")
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 const port = 3000
+
 /// On utilise bodyParser pour lire les données HTTP POST
 const bodyParser = require("body-parser")
-/// On utilise Multer pour récupérer les données envoyés du formulaire
-const multer = require("multer")
-//// Path pour récuperer les images
-const path = require("path")
-/// On utilise diskStorage pour récuperer le nom de notre image et qu'elle soit affiché dans son format dans le string des sauces
-const storage = multer.diskStorage({
-destination: "images/", 
-filename: function (req, file, cb) {
-    cb(null, imageFile(req, file))
-}
-})
-//// Fonction pour récupérer les imagees grâce à Multer et les renommer
-function imageFile(req, file) {
-    const fileName = `${Date.now()}-${file.originalname}`
-    file.fileName = fileName
-    return fileName
-}
 
-const upload = multer({ storage: storage })
 /// Connextion à mongo
 require("./mongo")
 
-/// Connextion aux fichiers controllers
-const { createUser, logUser } = require("./controllers/user")
-const { getSauces, createSauces, getSauceId, deleteSauceId, modifySauces, likeSauce } = require("./controllers/sauces")
+
 //Middleware
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extendre: true}))
 
-// Routes
-// INSCRIPTION DE L'USER 
-app.post('/api/auth/signup', createUser)
-app.post("/api/auth/login", logUser)
-app.get("/api/sauces", getSauces)
-app.post("/api/sauces", upload.single("image"), createSauces)
-app.get("/api/sauces/:id", getSauceId)
-app.delete("/api/sauces/:id", deleteSauceId)
-app.put("/api/sauces/:id", upload.single("image"), modifySauces)
-app.post("/api/sauces/:id/like", likeSauce)
-app.get('/', (req, res) => res.send('Test'))
 
+app.use(bodyParser.json())
+app.use("/api/sauces", saucesRouter)
+app.use("/api/auth", authRouter)
 
 ////Middleware pour que notre dossier images soit public et donc y afficher les images
 app.use("/images", express.static(path.join(__dirname, "images")))
-/// Listen
+/// On demande à l'app d'écouter sur le port 3000 et de renvoyer le message
 app.listen(port, () => console.log('Listening on port' + port))
-
